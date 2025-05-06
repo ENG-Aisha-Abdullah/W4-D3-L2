226beb3cd2b6b240ae96a2e6973ce878078e342e
Ayoosh <>
2025-05-06 13:49:30 +0300

    lab4

--- lab4.js
+++ lab4.js
@@ -1,7 +1,7 @@
 let saudiaAirline = {
     airlineName: "الخطوط الجوية السعودية",
     country: "السعودية",
-    fleetSize: 150,
+    fleetSize: 120,
     destinations: ["الرياض", "جدة", "دبي", "نيويورك"],
     inOperation: true,
     bookFlight: function(destination) {
@@ -11,10 +11,33 @@ let saudiaAirline = {
             console.log(`عذرًا، لا توجد رحلات إلى ${destination}`);
         }
     },
+    addDestination: function (newDestination) {
+        if (!this.destinations.includes(newDestination)) {
+            this.destinations.push(newDestination);
+            console.log(`${newDestination} اضافة وجهة جديدة`);
+        } else {
+            console.log(`${newDestination} الوجهة موجودة مسبقا`);
+        }
+    },
+    getSummary: function () {
+        console.log(
+            `${this.airlineName} تعمل في 
+            ${this.country} وتمتلك 
+            ${this.fleetSize} طائرة وتطير إلى 
+            ${this.destinations.length} وجهة `);
+    }  
 };
+
 
 console.log(saudiaAirline);
 
+saudiaAirline.bookFlight("دبي");
+// تم حجز رحلة إلى دبي.
+
+saudiaAirline.addDestination("لندن");
+
+saudiaAirline.getSummary();
+// الخطوط الجوية السعودية تعمل في السعودية وتمتلك 120 طائرة وتطير إلى 21 وجهة.
 
 
 
