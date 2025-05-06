49d4c9dd55aca5697a49534e7ebf1fde4ebf26bc
Ayoosh <>
2025-05-06 13:25:29 +0300

    lab4

---
+++ lab4.js
@@ -0,0 +1,19 @@
+let saudiaAirline = {
+    airlineName: "الخطوط الجوية السعودية",
+    country: "السعودية",
+    fleetSize: 150,
+    destinations: ["الرياض", "جدة", "دبي", "نيويورك"],
+    inOperation: true
+};
+
+console.log(saudiaAirline);
+
+bookFlight: function(destination) {
+    if (this.destinations.includes(destination)) {
+        console.log(`تم حجز رحلة إلى ${destination}.`);
+    } else {
+        console.log(`عذرًا، لا توجد رحلات إلى ${destination}`);
+    }
+};
+
+
