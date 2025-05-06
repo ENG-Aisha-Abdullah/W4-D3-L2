255a463ea5c75cc51c225b72409277a9efdb4f13
Ayoosh <>
2025-05-06 13:27:59 +0300

    lab4

--- lab4.js
+++ lab4.js
@@ -3,17 +3,18 @@ let saudiaAirline = {
     country: "السعودية",
     fleetSize: 150,
     destinations: ["الرياض", "جدة", "دبي", "نيويورك"],
-    inOperation: true
+    inOperation: true,
+    bookFlight: function(destination) {
+        if (this.destinations.includes(destination)) {
+            console.log(`تم حجز رحلة إلى ${destination}.`);
+        } else {
+            console.log(`عذرًا، لا توجد رحلات إلى ${destination}`);
+        }
+    },
 };
 
 console.log(saudiaAirline);
 
-bookFlight: function(destination) {
-    if (this.destinations.includes(destination)) {
-        console.log(`تم حجز رحلة إلى ${destination}.`);
-    } else {
-        console.log(`عذرًا، لا توجد رحلات إلى ${destination}`);
-    }
-};
+
 
 
